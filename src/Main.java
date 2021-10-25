64c571bf5bea8c4543f59c9813783862a59325f7
xingzhenyu21 <70183055+xingzhenyu21@users.noreply.github.com>
2021-10-25 20:19:06 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,24 @@
 
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PushbackReader;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
     public  static  ArrayList<Token> tokens = new ArrayList<Token>();
     public static void main(String[] args) throws IOException {
-        PushbackReader reader = new PushbackReader(new FileReader(args[0]),1024);
-        int ch;
-        Token test;
-        ch=reader.read();
-        while(ch!=-1) {
-            test = Func.getToken(ch,reader);
-            if(test == null)
-                break;
-
-            tokens.add(test);
-            ch= reader.read();
+        ArrayList<String> arrayList = new ArrayList<>();
+        try {
+            FileReader fr = new FileReader(args[0]);
+            BufferedReader bf = new BufferedReader(fr);
+            String str;
+            // 按行读取字符串
+            while ((str = bf.readLine()) != null) {
+                System.out.println(str);
+            }
+            bf.close();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-//        System.out.println(tokens.size());
-//        for(Token t:tokens){
-//            System.out.println(t.name);
-//        }
-        if(tokens.size()!=9)
-            System.exit(1);
-        Grammer grammer = new Grammer(args[1]);
-        grammer.CompUnit();
-        grammer.writer.close();
     }
 }
